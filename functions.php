1d78a4af74145ea02bdd6d92d99a42177447b684
pulchnymamut <pulchnymamut@gmail.com>
2022-03-26 20:14:46 +0100

    added custom search form

--- functions.php
+++ functions.php
@@ -27,7 +27,6 @@ function pustynnaburza_register_styles() {
 	$version = wp_get_theme()->get( 'Version' );
 	wp_enqueue_style('pustynnaburza-style', get_template_directory_uri() . "/style.css", array('pustynnaburza-bootstrap'), $version, 'all');
 	wp_enqueue_style('pustynnaburza-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
-	wp_enqueue_style('pustynnaburza-font', "https://fonts.googleapis.com/css?family=Open+Sans:400,600,300", array(), '1.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'pustynnaburza_register_styles');
