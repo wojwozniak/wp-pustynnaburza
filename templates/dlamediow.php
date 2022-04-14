cbedebe3e0d7974cb307b2f46fce610d68025bcf
pulchnymamut <pulchnymamut@gmail.com>
2022-04-14 10:01:15 +0200

    Added "in-build" adnotation

--- templates/dlamediow.php
+++ templates/dlamediow.php
@@ -4,6 +4,7 @@
 ?>
 <article id="dla-mediow" class="single-wrapper site">
 	<h1 class="site-title h1">Dla mediów</h1>
+	<p class="text normalplus">Zakładka w budowie :)</p>
 
 </article>
 <?php get_footer(); ?>
