aebb8072b40166d8d4f045d67105560493f1fd2b
pulchnymamut <pulchnymamut@gmail.com>
2022-06-24 17:56:46 +0200

    Second revision - 15 changes

---
+++ template-parts/social-links.php
@@ -0,0 +1,4 @@
+<ul class="text socials">
+    <li><a href="https://www.facebook.com/grapustynnaburza" target="_blank"><i class="fa-brands fa-facebook"></i>&nbsp;Facebook</a></li>
+    <li><a href="https://www.instagram.com/_pustynnaburza_" target="_blank"><i class="fa-brands fa-instagram"></i>&nbsp;Instagram</a></li>
+</ul>  
