909c20b8bd181b1299187c85eca4448c9d654ecc
pulchnymamut <pulchnymamut@gmail.com>
2022-07-08 19:14:27 +0200

    Made entire k-past blocks clickable + styling fixes

--- templates/kronika.php
+++ templates/kronika.php
@@ -12,70 +12,101 @@
 		</div>
 		<div class="k-past-container">
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/U15CRxm5iJkLUfxv8" target="_blank">2010</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb10.jpg" alt="">
+				<a class="normalplus" href="https://photos.app.goo.gl/U15CRxm5iJkLUfxv8" target="_blank">
+					<p class="normalplus">2010</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb10.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">2009</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb09.jpg" alt="">
+				<a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
+					<p class="normalplus">2009</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb09.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/C2eh62EeC4SEY1M96" target="_blank">2008</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb08.jpg" alt="">
+				<a href="https://photos.app.goo.gl/C2eh62EeC4SEY1M96" target="_blank">
+					<p class="normalplus">2008</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb08.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/R3tbTMHyzjQwPfUC6" target="_blank">2007</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb07.jpg" alt="">
-			</div>
-			<div class="k-past-container">
-			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/Bb2M5inzWN2T2Q9D9" target="_blank">2006</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb06.jpg" alt="">
+				<a href="https://photos.app.goo.gl/R3tbTMHyzjQwPfUC6" target="_blank">
+					<p class="normalplus">2007</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb07.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/BKt8m5QXQUvcLiVD7" target="_blank">2005</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb05.jpg" alt="">
+				<a href="https://photos.app.goo.gl/Bb2M5inzWN2T2Q9D9" target="_blank">
+					<p class="normalplus">2006</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb06.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/vXb8HtBuhL3i5nF7A" target="_blank">2004</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb04.jpg" alt="">
+				<a href="https://photos.app.goo.gl/BKt8m5QXQUvcLiVD7" target="_blank">
+					<p class="normalplus">2005</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb05.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/cGuzxcfTaJR1bdNA8" target="_blank">2003</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb03.jpg" alt="">
+				<a href="https://photos.app.goo.gl/vXb8HtBuhL3i5nF7A" target="_blank">
+					<p class="normalplus">2004</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb04.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/2HuuJ36wg5gnKFPa8" target="_blank">2002</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb02.jpg" alt="">
+				<a href="https://photos.app.goo.gl/cGuzxcfTaJR1bdNA8" target="_blank">
+					<p class="normalplus">2003</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb03.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/UrbBm6z4e9uRynfa6" target="_blank">2001</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb01.jpg" alt="">
+				<a href="https://photos.app.goo.gl/2HuuJ36wg5gnKFPa8" target="_blank">
+					<p class="normalplus">2002</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb02.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/wJLdB3LUZYoxiQWs5" target="_blank">2000</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb00.jpg" alt="">
+				<a href="https://photos.app.goo.gl/UrbBm6z4e9uRynfa6" target="_blank">
+					<p class="normalplus">2001</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb01.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/8BfCBEb5tkbFamNX6" target="_blank">1999</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb99.jpg" alt="">
+				<a href="https://photos.app.goo.gl/wJLdB3LUZYoxiQWs5" target="_blank">
+					<p class="normalplus">2000</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb00.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/mhKkybDFdb5SmbWQ6" target="_blank">1998</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb98.jpg" alt="">
+				<a href="https://photos.app.goo.gl/8BfCBEb5tkbFamNX6" target="_blank">
+					<p class="normalplus">1999</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb99.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/zFQsJib99vtR8Kgo6" target="_blank">1997</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb97.jpg" alt="">
+				<a href="https://photos.app.goo.gl/mhKkybDFdb5SmbWQ6" target="_blank">
+					<p class="normalplus">1998</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb98.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="https://photos.app.goo.gl/qJ6FiTovp3iMhGZB9" target="_blank">1996</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb96.jpg" alt="">
+				<a href="https://photos.app.goo.gl/zFQsJib99vtR8Kgo6" target="_blank">
+					<p class="normalplus">1997</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb97.jpg" alt="">
+				</a>
 			</div>
 			<div class="k-past">
-				<p class="normalplus"><a href="http://web.archive.org/web/20120202212354if_/http://www.czarna5.dabrowa.pl/pustynnaburza/" target="_blank">Archiwalna Strona</p>
-				<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/starelogo.jpg" alt="">
-		</div>
+				<a href="https://photos.app.goo.gl/qJ6FiTovp3iMhGZB9" target="_blank">
+					<p class="normalplus">1996</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/pb96.jpg" alt="">
+				</a>
+			</div>
+			<div class="k-past">
+				<a href="http://web.archive.org/web/20120202212354if_/http://www.czarna5.dabrowa.pl/pustynnaburza/" target="_blank">
+					<p class="normalplus">Archiwalna Strona</p>
+					<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/starelogo.jpg" alt="">
+				</a>
+			</div>
 	</div>
 </article>
 <?php get_footer(); ?>
