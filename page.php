f0b91bec157b225cbb2590845613f41434876b6b
pulchnymamut <pulchnymamut@gmail.com>
2022-07-08 23:49:26 +0200

    Default page theme added

--- page.php
+++ page.php
@@ -1,6 +1,13 @@
 <?php get_header(); ?>
-<article class="single-wrapper">
-	
+<article class="site">
+	<div class="wrapper">
+        <div class="title-wrapper">
+            <h2><?php wp_title(''); ?></h2>
+        </div>
+        <div>
+            <?php echo the_content(); ?>
+        </div>
+    </div>
 </article>
 	    
 <?php get_footer(); ?>
